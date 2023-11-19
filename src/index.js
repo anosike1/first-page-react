d0d6ea1f4f228191c9c12826b43fc74c4f9bdfb3
anosike1 <39166741+anosike1@users.noreply.github.com>
2023-11-19 18:31:07 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>My Favorite Foods</h1>
+    <ul>
+      <li>Eba and Egusi soup</li>
+      <li>Eba and Okro soup</li>
+      <li>Eba and any kind of sweet soup</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
